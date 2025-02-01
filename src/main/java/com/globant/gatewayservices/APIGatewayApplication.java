0b60bcc1a47978c1b2e947bbcc8dbb6f2f3402e5
Prashant Sonawane <prashant.sonawane@globant.com>
2025-02-01 17:53:44 +0530

    Update properties

--- src/main/java/com/globant/gatewayservices/APIGatewayApplication.java
+++ src/main/java/com/globant/gatewayservices/APIGatewayApplication.java
@@ -1,11 +1,20 @@
 package com.globant.gatewayservices;
 
+import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.netty.http.client.HttpClient;
 
 @SpringBootApplication
 public class APIGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(APIGatewayApplication.class, args);
     }
+
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+    }
 }
+
